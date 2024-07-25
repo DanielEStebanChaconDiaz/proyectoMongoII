66d54c6eaa0029359320d434f3b2485330074676
DanielEStebanChaconDiaz <138712462+DanielEStebanChaconDiaz@users.noreply.github.com>
2024-07-25 13:00:27 -0500

    feat: :bookmark: First version of admin crud

--- js/crud.js
+++ js/crud.js
@@ -1,29 +1,221 @@
 import readlineSync from 'readline-sync';
-import Connection from '../db/connect/connect.js';
+import { Movies } from './model/movies.js';
+import { Users } from './model/users.js';
 
-export async function conectarConMongoDB() {
-    const user = readlineSync.question('Por favor, ingresa tu nombre de usuario: ');
-    const pws = readlineSync.question('Por favor, ingresa tu contraseña: ', { hideEchoBack: true });
-
-    if (!user || !pws) {
-        console.error("El nombre de usuario y la contraseña son obligatorios.");
-        return;
+export class Login {
+    constructor() {
+        this.movies = new Movies();
+        this.users = new Users();
     }
 
-    const connection = new Connection();
-    await connection.login(user, pws);
+    async login() {
+        console.log("Bienvenido a la Base de Datos MongoDB");
+        const opciones = ['Administrador', 'Usuario Regular', 'Usuario VIP'];
+        const index = readlineSync.keyInSelect(opciones, '¿Cómo deseas conectarte?', {cancel: 'Salir'});
+        
+        switch(index) {
+            case 0:
+                await this.conectarComoAdministrador();
+                break;
+            case 1:
+                await this.conectarComoUsuario();
+                break;
+            case 2:
+                await this.conectarComoVip();
+                break;
+            default:
+                console.log("Saliendo del sistema...");
+        }
+    }
 
-    try {
-        console.log("Intentando conectar a MongoDB...");
-        const db = await connection.connect();
-        console.log("Conexión a MongoDB establecida correctamente.");
+    async conectarComoAdministrador() {
+        const user = readlineSync.question('Por favor, ingresa tu nombre de usuario: ');
+        const pws = readlineSync.question('Por favor, ingresa tu contraseña: ', { hideEchoBack: true });
+        console.clear();
+    
+        if (user === 'adminCineCampus' && pws === '1234') {
+            try {
+                console.log("Conectando a MongoDB...");
+                const resultPeliculas = await this.movies.getMovies(user, pws);
+                const resultDescription = await this.movies.getMoviesDescription(user, pws);
+                const resultUsers = await this.users.getUsers(user, pws);
+                console.log("Conexión a MongoDB establecida correctamente.");
+                await this.mostrarMenuAdministrador(user, pws, resultPeliculas, resultUsers, resultDescription);
+            } catch (error) {
+                console.error("Error al conectar a MongoDB:", error);
+            }
+        } else {
+            console.log("Nombre de usuario o contraseña incorrectos.");
+        }
+    }
 
-        // Ejemplo: Listar las colecciones
-        const collections = await db.listCollections().toArray();
-        console.log("Colecciones en la base de datos:", collections);
+    async mostrarMenuAdministrador(user, pws, resultPeliculas, resultUsers, resultDescription) {
+        let continuar = true;
 
-        await connection.close();
-    } catch (error) {
-        console.error("Error al conectar a MongoDB:", error);
+        while (continuar) {
+            const opciones = [
+                'Listar Películas',
+                'Listar Descripción de películas',
+                'Listar Usuarios',
+                'Agregar Película',
+                'Editar Película',
+                'Eliminar Película'
+            ];
+
+            const index = readlineSync.keyInSelect(opciones, '¿Qué acción deseas realizar?', {cancel: 'Salir'});
+
+            switch (index) {
+                case 0:
+                    console.log("Listando películas:");
+                    console.log(resultPeliculas);
+                    break;
+                case 1:
+                    console.log("Descripción de películas:");
+                    console.log(resultDescription);
+                    break;
+                case 2:
+                    console.log("Listando Usuarios:");
+                    console.log(resultUsers);
+                    break;
+                case 3:
+                    console.log("Agregar película:");
+                    await this.movies.agregarPelicula(user, pws);
+                    break;
+                case 4:
+                    // Implementar edición de película
+                    break;
+                case 5:
+                    await this.movies.dropMovie(user, pws);
+                    break;
+                default:
+                    continuar = false;
+                    console.log("Saliendo del menú de administrador...");
+            }
+
+            if (continuar) {
+                readlineSync.question('Presiona Enter para continuar...');
+            }
+        }
+    }
+
+    // ... (resto de los métodos sin cambios)
+
+    async conectarComoUsuario() {
+        const user = readlineSync.question('Por favor, ingresa tu nombre de usuario: ');
+        const pws = readlineSync.question('Por favor, ingresa tu contraseña: ', { hideEchoBack: true });
+        console.clear();
+
+        try {
+            // Aquí deberías verificar las credenciales del usuario en tu base de datos
+            console.log("Conectando como usuario regular...");
+            const resultPeliculas = await this.movies.getMovies(user, pws);
+            console.log("Conexión establecida correctamente.");
+            await this.mostrarMenuUsuario(resultPeliculas);
+        } catch (error) {
+            console.error("Error al conectar:", error);
+        }
+    }
+
+    async conectarComoVip() {
+        const user = readlineSync.question('Por favor, ingresa tu nombre de usuario VIP: ');
+        const pws = readlineSync.question('Por favor, ingresa tu contraseña: ', { hideEchoBack: true });
+        console.clear();
+
+        try {
+            // Aquí deberías verificar las credenciales del usuario VIP en tu base de datos
+            console.log("Conectando como usuario VIP...");
+            const resultPeliculas = await this.movies.getMovies(user, pws);
+            console.log("Conexión establecida correctamente.");
+            await this.mostrarMenuVip(resultPeliculas);
+        } catch (error) {
+            console.error("Error al conectar:", error);
+        }
+    }
+
+    async mostrarMenuUsuario(resultPeliculas) {
+        let continuar = true;
+
+        while (continuar) {
+            const opciones = [
+                'Ver Películas Disponibles',
+                'Comprar Boleto',
+                'Reservar Boleto',
+                'Cancelar Reservación'
+            ];
+
+            const index = readlineSync.keyInSelect(opciones, '¿Qué acción deseas realizar?', {cancel: 'Salir'});
+
+            switch (index) {
+                case 0:
+                    console.log("Películas Disponibles:");
+                    console.log(resultPeliculas);
+                    break;
+                case 1:
+                    await this.comprarBoleto();
+                    break;
+                case 2:
+                    await this.reservarBoleto();
+                    break;
+                case 3:
+                    await this.cancelarReservacion();
+                    break;
+                default:
+                    continuar = false;
+                    console.log("Cerrando sesión...");
+            }
+
+            if (continuar) {
+                readlineSync.question('Presiona Enter para continuar...');
+            }
+        }
+    }
+
+    async mostrarMenuVip(resultPeliculas) {
+        let continuar = true;
+
+        while (continuar) {
+            const opciones = [
+                'Ver Películas Disponibles',
+                'Comprar Boleto con Descuento VIP',
+                'Reservar Boleto',
+                'Cancelar Reservación',
+                'Ver Beneficios VIP'
+            ];
+
+            const index = readlineSync.keyInSelect(opciones, '¿Qué acción deseas realizar?', {cancel: 'Salir'});
+
+            switch (index) {
+                case 0:
+                    console.log("Películas Disponibles:");
+                    console.log(resultPeliculas);
+                    break;
+                case 1:
+                    await this.comprarBoletoVIP();
+                    break;
+                case 2:
+                    await this.reservarBoleto();
+                    break;
+                case 3:
+                    await this.cancelarReservacion();
+                    break;
+                case 4:
+                    this.verBeneficiosVIP();
+                    break;
+                default:
+                    continuar = false;
+                    console.log("Cerrando sesión...");
+            }
+
+            if (continuar) {
+                readlineSync.question('Presiona Enter para continuar...');
+            }
+        }
+    }
+
+    verBeneficiosVIP() {
+        console.log("Beneficios VIP:");
+        console.log("1. Descuento del 15% en todas las compras de boletos");
+        console.log("2. Acceso anticipado a estrenos");
+        console.log("3. Snacks gratis en cada visita");
     }
 }
---
+++ js/model/movies.js
@@ -0,0 +1,144 @@
+// models/movies.js
+import Connection from '../../db/connect/connect.js';
+import readlineSync from 'readline-sync';
+
+export class Movies {
+    constructor() {
+        this.connection = new Connection();
+        this.db = null;
+    }
+
+    async connect(user, pws) {
+        if (!this.db) {
+            try {
+                await this.connection.login(user, pws);
+                this.db = await this.connection.connect();
+            } catch (err) {
+                console.error('Error connecting to the database:', err);
+            }
+        }
+    }
+
+    async getMovies(user, pws) {
+        await this.connect(user, pws);
+        try {
+            const collections = await this.db.collection('movies').find().toArray();
+            return collections;
+        } catch (err) {
+            console.error('Error fetching movies:', err);
+        }
+    }
+
+    async getMoviesDescription(user, pws) {
+        await this.connect(user, pws);
+        try {
+            const collections = await this.db.collection('movies').aggregate([
+                {
+                    $lookup: {
+                        from: 'movie-description',
+                        localField: 'movieId',
+                        foreignField: 'movieId',
+                        as: 'Description'
+                    }
+                },
+                {
+                    $unwind: '$Description'
+                },
+                {
+                    $project: {
+                        Title: '$title',
+                        Description: '$Description.description',
+                        Genre: '$Description.genre',
+                        Duration: '$Description.duration'
+                    }
+                }
+            ]).toArray();
+            return collections;
+        } catch (err) {
+            console.error('Error fetching movie descriptions:', err);
+        }
+    }
+    async agregarPelicula(user, pws) {
+        await this.connect(user, pws);
+        try {
+            let movieId;
+            let existingMovie;
+            let title;
+            let existingMovieName;
+    
+            do {
+                movieId = parseInt(readlineSync.question('Ingrese el ID de la película: '));
+                existingMovie = await this.db.collection('movies').findOne({ movieId: movieId });
+                if (existingMovie) {
+                    console.log('Error: Ya existe una película con este ID. Inténtelo de nuevo.');
+                    continue;
+                }
+    
+                title = readlineSync.question('Ingrese el título de la película: ');
+                existingMovieName = await this.db.collection('movies').findOne({ title: title });
+                if (existingMovieName) {
+                    console.log('Error: Ya existe una película con este título. Inténtelo de nuevo.');
+                }
+            } while (existingMovie || existingMovieName);
+    
+            const rating = parseFloat(readlineSync.question('Ingrese el rating de la película (0-10): '));
+            const genre = readlineSync.question('Ingrese el género de la película: ');
+            const duration = parseInt(readlineSync.question('Ingrese la duración de la película (en minutos): '));
+            const description = readlineSync.question('Ingrese la descripción de la película: ');
+    
+            // Obtener la fecha y hora actual
+            const creationDate = new Date();
+    
+            const movieDocument = {
+                movieId: movieId,
+                title: title,
+                creationDate: creationDate, // Fecha y hora de creación del registro
+                rating: rating,
+            };
+    
+            const descriptionDocument = {
+                movieId: movieId,
+                description: description,
+                genre: genre,
+                duration: duration
+            };
+    
+            await this.db.collection('movies').insertOne(movieDocument);
+            await this.db.collection('movie-description').insertOne(descriptionDocument);
+    
+            console.log('Película agregada exitosamente.');
+            return `Película agregada: ${title} (Creada el ${creationDate.toLocaleString()})`;
+        } catch (err) {
+            console.error('Error al agregar la película:', err);
+            return 'Error al agregar la película: ' + err.message;
+        }
+    }
+    async dropMovie(user, pws) {
+        await this.connect(user, pws);
+        try {
+            const movieId = parseInt(readlineSync.question('Ingrese el ID de la película que desea eliminar: '));
+            const existingMovie = await this.db.collection('movies').findOne({ movieId: movieId });
+    
+            if (existingMovie) {
+                await this.db.collection('movies').deleteOne({ movieId: movieId });
+                await this.db.collection('movie-description').deleteOne({ movieId: movieId });
+                console.log('Película eliminada con éxito.');
+                return `Película con ID ${movieId} eliminada con éxito.`;
+            } else {
+                console.log('No se encontró la película con el ID ingresado.');
+                return `No se encontró la película con ID ${movieId}.`;
+            }
+        } catch (err) {
+            console.error('Error al eliminar la película:', err);
+            return 'Error al eliminar la película: ' + err.message;
+        }
+    }
+    
+    
+    async closeConnection() {
+        if (this.connection) {
+            await this.connection.close();
+            this.db = null;
+        }
+    }
+}
